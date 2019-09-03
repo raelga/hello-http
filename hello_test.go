a2cfda4adf595c3209a91fe21f72ad3c929c1f20
rael <rael@rael.io>
2019-09-03 20:22:26 +0200

    Add helloHandler tests

--- hello_test.go
+++ hello_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -33,6 +35,47 @@ func Test_helloName(t *testing.T) {
 			}
 			if got := helloName(tt.env.key); got != tt.want {
 				t.Errorf("helloName(\"%v\") = %v, want %v", tt.env.key, got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_helloHandler(t *testing.T) {
+	type testReq struct {
+		path     string
+		httpCode int
+	}
+	tests := []struct {
+		name string
+		want string
+		req  testReq
+	}{
+		{name: "rootWorldRequest", want: "Hello World!", req: testReq{path: "/", httpCode: 200}},
+		{name: "fakePathRequest", want: "Hello World!", req: testReq{path: "/fakePathRequest", httpCode: 200}},
+	}
+
+	for _, tt := range tests {
+
+		// Create the test HTTP request
+		req, err := http.NewRequest("GET", tt.req.path, nil)
+		if err != nil {
+			t.Errorf("main() NewRequest failed: %v", err)
+		}
+
+		// Execute the HTTP request
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(helloHandler)
+		handler.ServeHTTP(rr, req)
+
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rr.Code; got != tt.req.httpCode {
+				t.Errorf("helloHandler() Status = %v, want %v", got, tt.want)
+			}
+		})
+
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rr.Body.String(); got != tt.want {
+				t.Errorf("helloHandler() Body = %v, want %v", got, tt.want)
 			}
 		})
 	}
